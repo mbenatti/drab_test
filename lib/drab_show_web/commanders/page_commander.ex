8a9b06d53b0b5cc56346d21efd0d78cb9a701575
Marcos Benatti Lauer <benatti77@gmail.com>
2018-01-31 19:44:15 -0200

    bug

--- lib/drab_show_web/commanders/page_commander.ex
+++ lib/drab_show_web/commanders/page_commander.ex
@@ -1,5 +1,8 @@
 defmodule DrabShowWeb.PageCommander do
   use Drab.Commander
+
+  import Phoenix.HTML, only: [sigil_E: 2, safe_to_string: 1]
+
   # Place your event handlers here
   #
   # def button_clicked(socket, sender) do
@@ -13,4 +16,26 @@ defmodule DrabShowWeb.PageCommander do
   # def page_loaded(socket) do
   #   set_prop socket, "div.jumbotron h2", innerText: "This page has been drabbed"
   # end
+
+  def change_select_1(socket, sender) do
+    html = ~E"""
+            <option>Changed select 1</option>
+            <option>Changed select 1(2)</option>
+          """
+          |> safe_to_string()
+
+    poke socket, welcome_text: "Changed by change_select_1"
+    set_prop socket, "select:nth-child(1)", innerHTML: html
+  end
+
+  def change_select_2(socket, sender) do
+    html = ~E"""
+              <option>Changed select 2</option>
+              <option>Changed select 2(2)</option>
+            """
+          |> safe_to_string()
+
+    poke socket, welcome_text: "Changed by change_select_2"
+    set_prop socket, "select:nth-child(2)", innerHTML: html
+  end
 end
--- lib/drab_show_web/router.ex
+++ lib/drab_show_web/router.ex
@@ -17,6 +17,7 @@ defmodule DrabShowWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
   end
 
   # Other scopes may use custom stacks.
