a3aad58a11aa3822cc157f4a3f69c4a5a7cdff8a
Marcos Benatti Lauer <benatti77@gmail.com>
2018-01-31 18:55:15 -0200

    drab setup

--- lib/drab_show_web/controllers/page_controller.ex
+++ lib/drab_show_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule DrabShowWeb.PageController do
   use DrabShowWeb, :controller
+  use Drab.Controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html",
+    welcome_text: "Welcome to Phoenix!"
   end
 end
